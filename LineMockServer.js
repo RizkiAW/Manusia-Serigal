f725b121471d91fab78eb1f478a413c2a56becf6
ar46fuck <31289048+ar46fuck@users.noreply.github.com>
2017-09-05 00:14:28 +0800

    Update LineMockServer.js

--- LineMockServer.js
+++ LineMockServer.js
@@ -9,13 +9,11 @@ app.get('/', function(req, res){
 });
 
 app.post('/message/reply',function(req, res){
-    showTime();
     console.dir(req.body);
     res.json(req.body);
 });
 
 app.post('/message/push', function(req, res){
-    showTime();
     console.dir(req.body);
     res.json(req.body);
 });
@@ -64,71 +62,14 @@ app.get('/profile/:userId', function(req, res){
             'pictureUrl': 'http://placeholder.it/256x256',
             'statusMessage': 'exampleStatusMessage',
         },
-        'member_8':{
-            'displayName': 'Mrs Puff',
-            'userId': 'member_8',
-            'pictureUrl': 'http://placeholder.it/256x256',
-            'statusMessage': 'exampleStatusMessage',
-        },
-        'member_9':{
-            'displayName': 'Gary the snails',
-            'userId': 'member_9',
-            'pictureUrl': 'http://placeholder.it/256x256',
-            'statusMessage': 'exampleStatusMessage',
-        },
-        'member_10':{
-            'displayName': 'Squilliam',
-            'userId': 'member_10',
-            'pictureUrl': 'http://placeholder.it/256x256',
-            'statusMessage': 'exampleStatusMessage',
-        },
-        'member_11':{
-            'displayName': 'Pearl',
-            'userId': 'member_11',
-            'pictureUrl': 'http://placeholder.it/256x256',
-            'statusMessage': 'exampleStatusMessage',
-        },
-        'member_12':{
-            'displayName': 'Flying dutchman',
-            'userId': 'member_12',
-            'pictureUrl': 'http://placeholder.it/256x256',
-            'statusMessage': 'exampleStatusMessage',
-        },
-        'member_13':{
-            'displayName': 'Neptunus',
-            'userId': 'member_13',
-            'pictureUrl': 'http://placeholder.it/256x256',
-            'statusMessage': 'exampleStatusMessage',
-        },
-        'member_14':{
-            'displayName': 'Karen robot',
-            'userId': 'member_14',
-            'pictureUrl': 'http://placeholder.it/256x256',
-            'statusMessage': 'exampleStatusMessage',
-        },
-        'member_15':{
-            'displayName': 'Mermaid man',
-            'userId': 'member_15',
-            'pictureUrl': 'http://placeholder.it/256x256',
-            'statusMessage': 'exampleStatusMessage',
-        },
-        'member_16':{
-            'displayName': 'Barnacle boys',
-            'userId': 'member_16',
-            'pictureUrl': 'http://placeholder.it/256x256',
-            'statusMessage': 'exampleStatusMessage',
-        },
     };
 
     var selectedUser = users[req.params.userId];
-    showTime();
-    console.log("Returning profile for userId = "+req.params.userId);
     console.dir(selectedUser);
     res.json(selectedUser);
 });
 
 app.post('*', function(req, res){
-    showTime();
     console.dir(req.body);
     res.json(req.body);
 });
@@ -136,13 +77,3 @@ app.post('*', function(req, res){
 app.listen(8000, function(){
     console.log("listening on port 8000");
 });
-
-function showTime(){
-    var options = {
-        timeZone: 'Asia/Jakarta',
-        year: 'numeric', month: 'numeric', day: 'numeric',
-        hour: 'numeric', minute: 'numeric', second: 'numeric',
-    },
-    formatter = new Intl.DateTimeFormat([], options);
-    console.log(formatter.format(new Date()));
-}
